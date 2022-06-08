56ce89b59036ff7f04bf83b417fc8a143ba71fa6
40231778 <jgregg10@qub.ac.uk>
2022-06-08 20:12:54 +0100

    Return perfect matching eyeshadow colour from colourwheel based on eye colour

--- eyecolourwheel/quickstart/views.py
+++ eyecolourwheel/quickstart/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from eyecolourwheel.quickstart.serializers import EyeColourSerializer
 from eyecolourwheel.quickstart.models import EyeColour
 from rest_framework.viewsets import ModelViewSet, GenericViewSet
-
+from django.http.response import JsonResponse
 # Create your views here.
 # Get all eye colours
 
@@ -13,3 +13,16 @@ class EyeColourList(ModelViewSet):
 
     def get_serializer_context(self):
         return {'request': self.request}
+
+    def create(self, request, *args, **kwargs):
+        print(request.POST['eyeColour'])
+        if request.POST['eyeColour'] == 'HZ':
+            colourMatch = "Brown"
+        elif request.POST['eyeColour'] == 'BLU':
+            colourMatch = "Orange"
+        elif request.POST['eyeColour'] == 'BRW':
+            colourMatch = "Pink"
+        elif request.POST['eyeColour'] == 'GR':
+            colourMatch = "Blue"
+        super().create(request, *args, **kwargs)
+        return JsonResponse({'matches': colourMatch})
